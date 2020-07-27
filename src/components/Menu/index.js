b44fc4e32216a30103fbef30b440564ba79d18b2
Gabriel Gomes de Oliveira <gabrielogomes1@gmail.com>
2020-07-27 11:33:42 -0300

    Started the challenge

---
+++ src/components/Menu/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+// import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+import "./Menu.css"
+
+function Menu() {
+    return (
+        <nav className="Menu">
+            <a href='/'>
+                <img className="Logo" src={Logo} alt='AluraFlix logo' />
+            </a>
+
+            <Button as='a' className = "ButtonLink" href="/">
+                Novo Video
+            </Button>
+        </nav>
+
+
+
+    );
+}
+
+export default Menu; //  importa o menu
